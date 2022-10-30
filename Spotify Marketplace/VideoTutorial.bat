640c1610de501bbe24ceedd8d5d8571e021e487f
franblackkk <88139895+franblackkk@users.noreply.github.com>
2022-10-30 14:27:11 +0100

    ArvelTutorials

---
+++ Spotify Marketplace/VideoTutorial.bat
@@ -0,0 +1,12 @@
+@echo off
+
+title Autore: ArvelTv - Pagina iniziale
+color c
+echo ______________
+echo Autore: ArvelTv
+echo Programma: Spotify
+echo ______________
+echo .
+echo Il tutorial non e disponibile... chiudendo la finestra..
+timeout 3
+exit
