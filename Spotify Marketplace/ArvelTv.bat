febb0437964e6c243a683f2f1f572353f7578756
franblackkk <88139895+franblackkk@users.noreply.github.com>
2022-10-30 14:11:03 +0100

    ArvelTutorials

---
+++ Spotify Marketplace/ArvelTv.bat
@@ -0,0 +1,19 @@
+@echo off
+title Autore: ArvelTv - Pagina iniziale
+color c
+echo ______________
+echo Autore: ArvelTv
+echo Programma: Spotify
+echo ______________
+echo .
+echo Eseguire Questo File? Se no Chiudere la finestra, Se si allora
+pause
+cls
+echo Sto eseguendo il comando. Non chiudere questa finestra!
+title title Autore: ArvelTv - Esecuzione Comando In corso... - NON CHIUDERE QUESTA FINESTRA!
+powershell -Command "Invoke-WebRequest -UseBasicParsing "https://raw.githubusercontent.com/spicetify/spicetify-marketplace/main/resources/install.ps1" | Invoke-Expression"
+cls
+title Autore: ArvelTv - Processo finito.
+Echo Processo finito, Questa finestra sara chiusa tra 5 secondi...
+timeout 5
+exit
